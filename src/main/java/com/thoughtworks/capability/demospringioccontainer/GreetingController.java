8360b2ad050f65bfbca21c4cbc60c0c528872b93
chentaiyu0336 <2868510447@qq.com>
2020-09-16 22:38:45 +0800

    [cty] solve problem 2

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,21 +1,31 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
 
 @RestController
 public class GreetingController {
 
-    private final GreetingService greetingService;
+//    private final GreetingService greetingService;
 
     @Autowired
-    public GreetingController(GreetingService greetingService) {
-        this.greetingService = greetingService;
-    }
+    private BeanFactory beanFactory;
+
+//    @Autowired
+//    @Lazy
+//    public GreetingController(GreetingService greetingService) {
+//        this.greetingService = greetingService;
+//    }
 
     @GetMapping("/greet")
     public String greet() {
+        GreetingService greetingService = beanFactory.getBean(GreetingService.class);
         return greetingService.sayHi();
     }
 
